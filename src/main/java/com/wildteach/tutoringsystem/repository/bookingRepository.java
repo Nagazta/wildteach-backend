2f58c999be8c4b97f3ac903fbcb2a5fdeee5457d
juvie <juvie.coca@cit.edu>
2025-04-19 00:27:34 +0800

    added bookingRepository

---
+++ src/main/java/com/wildteach/tutoringsystem/repository/bookingRepository.java
@@ -0,0 +1,11 @@
+package com.wildteach.tutoringsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wildteach.tutoringsystem.entity.bookingEntity;
+
+@Repository
+public interface bookingRepository extends JpaRepository<bookingEntity, Long> {
+}
+
